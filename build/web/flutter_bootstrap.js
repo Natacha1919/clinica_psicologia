0819f58365184b252f060fc0e6cbe7828c70b25d
Natacha <natacha.bezerra@fecaf.com.br>
2025-10-27 10:44:32 -0300

    Finalizando últimos ajustes

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3551824143"
+    serviceWorkerVersion: "1171966545"
   }
 });
--- lib/features/triagem/models/status_paciente.dart
+++ lib/features/triagem/models/status_paciente.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
 enum StatusPaciente {
-  atendimentoEncerrado('ATENDIMENTO ENCERRADO', Color.fromARGB(255, 0, 17, 255)),
+  atendimentoEncerrado(
+      'ATENDIMENTO ENCERRADO', Color.fromARGB(255, 0, 17, 255)),
   emAtendimento('EM ATENDIMENTO', Colors.teal),
   triagemRealizada('TRIAGEM REALIZADA', Color.fromARGB(255, 68, 255, 143)),
   espera('EM ESPERA', Colors.amber),
-  filaDeEsperaRemanescente('FILA DE ESPERA REMANESCENTE', const Color(0xFFD32F2F)); // Corrigido para ser constante
+  filaDeEsperaRemanescente('FILA DE ESPERA REMANESCENTE',
+      const Color(0xFFD32F2F)); // Corrigido para ser constante
 
   const StatusPaciente(this.valor, this.cor);
   final String valor;
